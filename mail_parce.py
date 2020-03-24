3058342dfcb337b654ae542f0d9f8b703667ae8f
YuriiShutko <yurayurban72@gmail.com>
2020-03-24 21:12:56 +0200

    Made some comments in mail_parce module

--- mail_parce.py
+++ mail_parce.py
@@ -3,7 +3,12 @@ import gmail_get
 
 
 def get_data_from_text(text):
-    regex = '.*(?P<date>\d\d\.\d\d\.\d\d\d\d).*новый сотрудник (?P<name>.*) \(.*подразделение (?P<department>.*) на должность (?P<role>.*)\..*Руководитель - (?P<manager>.*)\..*Офис'
+
+    """Function that help to filter a message using regex pattern and collect some values to variables.
+    Output is a dict of data"""
+
+    regex = '.*(?P<date>\d\d\.\d\d\.\d\d\d\d).*новый сотрудник (?P<name>.*) \(.*подразделение (?P<department>.*) на ' \
+            'должность (?P<role>.*)\..*Руководитель - (?P<manager>.*)\..*Офис '
     match = re.match(regex, text, re.DOTALL)
     return {
         'date': match.group('date'),
@@ -15,6 +20,9 @@ def get_data_from_text(text):
 
 
 def get_array_of_data():
+
+    """Function helps to collect all filtered data in one array and it returns that"""
+
     array_of_data = []
     array_of_messages = gmail_get.get_bodies_of_messages()
     for msg in array_of_messages:
